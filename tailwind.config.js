979c9cb0dd4ea4878932996eaea50f0bf168dc0a
Alprod <41442860+Alprod@users.noreply.github.com>
2023-11-04 00:55:30 +0100

    Config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+      './assets/**/*.{js,jsx}',
+      './templates/**/*.html.twig'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
