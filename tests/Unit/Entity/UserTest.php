3e6a0c3a66639a725a2e5b6c5d7b0976b281568e
Alprod <41442860+Alprod@users.noreply.github.com>
2023-11-24 00:30:13 +0100

    creation test unitaire user entité

--- tests/Unit/Entity/UserTest.php
+++ tests/Unit/Entity/UserTest.php
@@ -2,8 +2,31 @@
 
 namespace App\Tests\Unit\Entity;
 
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+    private $user;
+
+    protected function setUp(): void
+    {
+        $this->user = new User();
+    }
+
+    public function testSetAndGetEmail(): void
+    {
+        $email = 'JohnDoe@demo.fr';
+        $this->user->setEmail($email);
+
+        $this->assertEquals($email, $this->user->getEmail());
+    }
+
+    public function testSetAndGetRole(): void
+    {
+        $role = ['ROLE_USER'];
+        $this->user->setRoles($role);
+        self::assertIsArray($this->user->getRoles());
+        self::assertContains('ROLE_USER', $this->user->getRoles());
+    }
 }
