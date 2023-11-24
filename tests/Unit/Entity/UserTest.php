b895ce27543744890da4f7de480975d0be6b0cdc
Alprod <41442860+Alprod@users.noreply.github.com>
2023-11-24 01:56:42 +0100

    Update UserTest.php
    Vérification des Setters et des Getters

--- tests/Unit/Entity/UserTest.php
+++ tests/Unit/Entity/UserTest.php
@@ -2,16 +2,23 @@
 
 namespace App\Tests\Unit\Entity;
 
+use App\Entity\Binomial;
+use App\Entity\Client;
 use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    private $user;
+    private User $user;
 
     protected function setUp(): void
     {
         $this->user = new User();
+    }
+
+    public function testInstenceOf(): void
+    {
+        self::assertInstanceOf(User::class, $this->user);
     }
 
     public function testSetAndGetEmail(): void
@@ -28,5 +35,40 @@ class UserTest extends TestCase
         $this->user->setRoles($role);
         self::assertIsArray($this->user->getRoles());
         self::assertContains('ROLE_USER', $this->user->getRoles());
+    }
+
+    public function testSetAndGetCreatedAt(): void
+    {
+        $date = new \DateTimeImmutable('now');
+        $this->user->setCreatedAt($date);
+        self::assertEquals($date, $this->user->getCreatedAt());
+    }
+
+    public function testSetAndGetUserClient(): void
+    {
+        $client = new Client();
+        $name = 'chanel';
+        $user = $this->user;
+
+        $user->setClient($client);
+        $user->getClient()->setName($name);
+
+        self::assertNotNull($user->getClient());
+        self::assertEquals($name, $user->getClient()->getName());
+    }
+
+    public function testSetAndGetFirstBinomialAndSecondBinomial(): void
+    {
+        $binomial = new Binomial();
+        $user = $this->user;
+        $user->addFirstBinomial($binomial);
+        $user->addSecondBinomial($binomial);
+
+        self::assertNotNull($user->getFirstBinomials());
+        self::assertNotNull($user->getSecondBinomials());
+
+        self::assertIsObject($user->getFirstBinomials());
+        self::assertIsObject($user->getSecondBinomials());
+
     }
 }
