9596a610cd76b6334a5bd8d28d5df2620fde582d
Alprod <41442860+Alprod@users.noreply.github.com>
2023-12-30 16:29:44 +0100

    Update AuthenticationFailedHandler.php
    modification des reponses apporter pour l'utilisateur

--- src/Security/Authentication/AuthenticationFailedHandler.php
+++ src/Security/Authentication/AuthenticationFailedHandler.php
@@ -2,13 +2,16 @@
 
 namespace App\Security\Authentication;
 
+use App\Entity\User;
 use App\Logger\SecurityLogger;
 use App\Service\MessageGeneratorService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 
 readonly class AuthenticationFailedHandler implements AuthenticationFailureHandlerInterface
@@ -16,7 +19,8 @@ readonly class AuthenticationFailedHandler implements AuthenticationFailureHandl
     public function __construct(
         private SecurityLogger $securityLogger,
         private UrlGeneratorInterface $urlGenerator,
-        private MessageGeneratorService $messageGenerator
+        private MessageGeneratorService $messageGenerator,
+        private EntityManagerInterface $entity
     ) {
     }
 
@@ -25,7 +29,17 @@ readonly class AuthenticationFailedHandler implements AuthenticationFailureHandl
      */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
     {
+        $em = $this->entity;
         $session = $request->getSession();
+        $reqUser = $request->request->get('_username');
+
+        $user = $em->getRepository(User::class)->findOneBy(['email' => $reqUser]);
+
+        if (!$user) {
+            $session->getFlashBag()->add('danger', message: $this->messageGenerator->getErrorAuthenticationLogin());
+
+            throw new UserNotFoundException('Utilisateur non trouvé');
+        }
         $route = $this->urlGenerator->generate('app_login');
         $this->securityLogger->securityErrorLog('Tantative de connexion echoué', [
             'messege' => $exception->getMessage(),
