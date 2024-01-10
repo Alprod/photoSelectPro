a23db98c9cdf50b3541c2665aa63717d4bbaf1a1
Alprod <41442860+Alprod@users.noreply.github.com>
2024-01-10 16:38:49 +0100

    Update ClientController.php
    pour compléter le profile client

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,32 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Client;
+use App\Entity\Identity;
 use App\Entity\User;
 use App\Form\ClientType;
+use App\Repository\UserRepository;
 use App\Security\EmailVerifier;
+use App\Service\EmailService;
 use App\Service\TimingTaskService;
 use App\Service\ToolsService;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/client')]
 class ClientController extends AbstractController
 {
     public function __construct(
         readonly private ToolsService $toolsService,
-        readonly private TimingTaskService $timingTaskService,
-        readonly private EmailVerifier $emailVerifier
-        )
-    {
-
+        readonly private TimingTaskService $timingTaskService
+    ) {
     }
-        
 
     #[Route('/', name: 'app_client')]
     public function index(): Response
@@ -39,15 +35,31 @@ class ClientController extends AbstractController
     }
 
     #[Route('/register_client', name: 'app_register_client')]
-    public function registerClient(Request $request, UserPasswordHasherInterface $passwordHasher): Response
-    {
-        $client = new Client;
+    public function registerClient(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        UserRepository $userRepo,
+        EmailService $emailService
+    ): Response {
+        $client = new Client();
         $user = new User();
+        $identity = new Identity();
+
         $form = $this->createForm(ClientType::class, $client);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $nameClient = $form->get('name')->getData();
+            $emailClient = $form->get('contactEmail')->getData();
+            $usernameClient = $form->get('contactName')->getData();
+            $userExist = $userRepo->findOneBy(['email' => $emailClient]);
+            $identityClient = explode(' ', $usernameClient);
+
+            if ($userExist) {
+                $this->addFlash('danger', 'Cette email '.$emailClient.' existe déjà.');
+
+                return $this->redirectToRoute('app_register_client');
+            }
 
             $refNumber = $this->toolsService->getGeneratorRefNumber($nameClient);
             $slug = $this->toolsService->getGeneratorSlugger($nameClient);
@@ -57,7 +69,7 @@ class ClientController extends AbstractController
                 ->setSlug($slug);
             $this->timingTaskService->timingEntityManager('Register client', Client::class, $client);
 
-            $user->setEmail($form->get('contactEmail')->getData())
+            $user->setEmail($emailClient)
                 ->setRoles(['ROLE_CLIENT'])
                 ->setClient($client)
                 ->setPassword($passwordHasher->hashPassword(
@@ -66,17 +78,20 @@ class ClientController extends AbstractController
                 ));
             $this->timingTaskService->timingEntityManager('Register user by client', User::class, $user);
 
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user, (new TemplatedEmail())
-                ->from(new Address('no-reply@teampsp.fr', '�Team PhotoSelectPro'))
-                ->to($user->getEmail())
-                ->subject('Veuillez confirmer votre email')
-                ->htmlTemplate('emails/confirmation_email.html.twig')
-            );
-            return $this->redirectToRoute('app_render_verif_email', [ 'email' => $user->getEmail() ]);
+            $identity
+                ->setUserIdentity($user)
+                ->setFirstname($identityClient[1])
+                ->setLastname($identityClient[0]);
+
+            $this->timingTaskService->timingEntityManager('Add identity by client', Identity::class, $identity);
+
+            $emailService->emailVerifierService($user);
+
+            return $this->redirectToRoute('app_render_verif_email', ['email' => $user->getEmail()]);
         }
 
         return $this->render('client/register_client.html.twig', [
-            'clientForm' => $form
+            'clientForm' => $form,
         ]);
     }
 }
